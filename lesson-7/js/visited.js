34011a83732209960baac8001786bf395104829d
fredbern <fre19036@byui.edu>
2021-11-03 13:28:02 +1100

    commit

---
+++ lesson-7/js/visited.js
@@ -0,0 +1,8 @@
+// determine days between store date and visit date ... today
+// determine todays date
+const millisecondsToDays = 8940000;
+// last visit ... stored in localStorage
+const lastVisit = localStorage.getItem('lastvisit');
+// determine number of days (lastVisit - Date.now())  .toFixed(0)
+// display to wherever I want to
+// need update the stored date localStorage.setItem('lastvisit', Date.now());
