8e2060358a9cecd7a8e6d006b1348bed6c070679
fredbern <fre19036@byui.edu>
2021-11-07 11:49:02 +1100

    update

---
+++ lesson-8/js/WebFontLoad.js
@@ -0,0 +1,4 @@
+        WebFont.load({google: {families: ["EB+Garamond:ital"]}})
+        WebFont.load({google: {families: ["Ephesis&family"]}})
+        WebFont.load({google: {families: ["Inconsolata"]}})
+        WebFont.load({google: {families: ["Italianno"]}})
