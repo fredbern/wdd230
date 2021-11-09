f7cab33c688c7eadc3ecbe4a175a46b289acc636
fredbern <fre19036@byui.edu>
2021-11-10 00:54:49 +1100

    update prophets

---
+++ lesson-9/js/prophets.js
@@ -0,0 +1,86 @@
+
+
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function (jsonObject) {
+        console.table(jsonObject); // temporary checking for valid response and data parsing
+        const prophets = jsonObject['prophets'];
+
+        for (let i = 0; i < prophets.length; i++) {
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let span = document.createElement('span');
+            let dateOfBirth = document.createElement('p');
+            let placeOfBirth = document.createElement('p');
+            let picture = document.createElement('picture');
+            let image = document.createElement('img');
+            image.setAttribute('data-src', prophets[i].imageurl);
+            image.setAttribute('src', 'images/placeholder.jpg');
+            image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname);
+        
+
+            //set my classes for my css
+            card.setAttribute('class', "section");
+            h2.setAttribute('class', 'section-h2');
+            span.setAttribute('class', 'title');
+            dateOfBirth.setAttribute('class', 'section-dateOfBirth');
+            placeOfBirth.setAttribute('class', 'section-placeOfBirth');
+            image.setAttribute('class', 'section-image');
+            
+
+
+
+            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            dateOfBirth.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+            placeOfBirth.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+
+
+            card.appendChild(h2);
+            card.appendChild(dateOfBirth);
+            card.appendChild(placeOfBirth);
+            card.appendChild(image);
+            document.querySelector('div.cards').appendChild(card);
+        }
+
+
+
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+const imgOptions = {
+    threshold: 1
+};
+//if this is true
+if ('IntersectionObserver' in window) {
+    // is supported so let's go!!!
+    const imgObserver = new IntersectionObserver((items) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    // load image if necessary
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+} else { //just load all images if not supported
+    imagesToLoad.forEach( (img) => {
+        loadImages(img);
+    });
+}
+
+});
+ 
